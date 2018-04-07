371d50f9e137c42c77b686eaa00940068edbee20
Andrey Breslav <andrey.breslav@jetbrains.com>
2018-04-07 20:03:08 +0300

    package.json

---
+++ main.js
@@ -0,0 +1,9 @@
+module.exports = (config) => {
+    return {
+        mongoStorage: require('engine/mongo-storage')(config),
+        BotEngine: require('engine/bot-engine'),
+        mailer: require('engine/mailgun-mailer')(config),
+        Scheduler: require('engine/scheduler')(config),
+        facebook: require('engine/facebook')(config)
+    }
+}
