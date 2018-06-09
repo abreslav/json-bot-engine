6aa0f6613e4d9e717a0aaafded6a0413a6dd499a
knize <kirill.knize@gmail.com>
2018-06-09 22:51:32 +0300

    revert instant response

--- engine/telegram.js
+++ engine/telegram.js
@@ -20,17 +20,13 @@ module.exports = (config) => {
         telegramSetWebhook()
 
         app.post(path, function (req, res) {
-            // handleRequest(req, engine)
-            //     .then(() =>
-            res.sendStatus(200)
-            //)
+            handleRequest(req, engine)
+                .then(() => res.sendStatus(200))
         })
 
         app.get(path, function (req, res) {
-            // handleRequest(req, engine)
-            //     .then(() =>
-            res.sendStatus(200)
-            // )
+            handleRequest(req, engine)
+                .then(() => res.sendStatus(200))
         })
 
         engine.initEngine(TelegramApi)
@@ -53,7 +49,7 @@ module.exports = (config) => {
         let c
         if (message) {
             c = context(message.chat.id)
-        } else if (callback_query) {
+        } else if(callback_query){
             c = context(callback_query.from.id)
         }
         if (message && message.text) {
@@ -94,7 +90,7 @@ module.exports = (config) => {
             extend(json, load, {chat_id: chat_id})
             await telegramPost("https://api.telegram.org/bot" + config.telegram.bot_token + "/sendMessage", json)
         }
-        this.sendphoto = async function (pic_url, text) {
+        this.sendphoto= async function (pic_url, text){
             let json = {
                 chat_id: chat_id,
                 photo: pic_url,
@@ -208,6 +204,7 @@ module.exports = (config) => {
     }
 
 
+
     function message(chat_id, text, paramsJSON) {
         return extend({chat_id: chat_id}, {text: text}, paramsJSON)
     }
@@ -217,6 +214,5 @@ module.exports = (config) => {
             goto: goto
         }, extra))
     }
-
     return result
 }
