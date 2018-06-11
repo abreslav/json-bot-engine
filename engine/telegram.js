7f70ae94b519824121b43a963a1abda4b797eff7
knize <kirill.knize@gmail.com>
2018-06-11 15:55:35 +0300

    try catch around handle req 1

--- engine/telegram.js
+++ engine/telegram.js
@@ -29,7 +29,7 @@ module.exports = (config) => {
                         err => console.log(err)
                     )
             } catch (e) {
-
+                console.log(e)
             }
 
         })
